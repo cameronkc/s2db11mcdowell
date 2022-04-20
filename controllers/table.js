18f337b0408e4a64b7052736dbae7c4772f7e8d5
cameronKC <cameronleekcmo@gmail.com>
2022-04-20 15:45:13 -0500

    table delete and updated methods added

--- controllers/table.js
+++ controllers/table.js
@@ -145,3 +145,31 @@ exports.table_create_Page =  function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 }; 
+
+// Handle building the view for updating a costume. 
+// query provides the id 
+exports.table_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Table.findById(req.query.id) 
+        res.render('tableupdate', { title: 'Table Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.table_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Table.findById(req.query.id) 
+        res.render('tableDelete', { title: 'Table Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
--- routes/table.js
+++ routes/table.js
@@ -13,6 +13,12 @@ router.get('/detail', table_controllers.table_view_one_Page);
 /* GET create table page */ 
 router.get('/create', table_controllers.table_create_Page); 
 
+/* GET create update page */ 
+router.get('/update', table_controllers.table_update_Page); 
+ 
+/* GET delete table page */ 
+router.get('/delete', table_controllers.table_delete_Page); 
+
 module.exports = router;
 
 router.get('/', table_controllers.table_view_all_Page ); 
